ddd923f8259bf41ab3b2ecf8f6a197f443e6938b
EazyFTW <Evan.Zimmerman@comcast.net>
2020-11-25 00:25:15 -0600

    Fixes, hopefully.

--- src/main/java/com/eazyftw/websitescreenshotter/browser/VirtualBrowser.java
+++ src/main/java/com/eazyftw/websitescreenshotter/browser/VirtualBrowser.java
@@ -5,16 +5,23 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeDriverService;
 import org.openqa.selenium.chrome.ChromeOptions;
 
-public class VirtualBrowser {
+public class VirtualBrowser extends Thread {
 
     protected ChromeDriver browser;
 
     public VirtualBrowser() {
+        start();
+    }
+
+    @Override
+    public void run() {
         WebDriverManager.chromedriver().setup();
         System.setProperty(ChromeDriverService.CHROME_DRIVER_SILENT_OUTPUT_PROPERTY, "true");
 
         ChromeOptions options = new ChromeOptions();
+        options.addArguments("--no-sandbox");
         options.addArguments("--headless");
+        options.addArguments("--disable-dev-shm-usage");
         options.addArguments("--window-size=1920x1080");
 
         this.browser = new ChromeDriver(options);
